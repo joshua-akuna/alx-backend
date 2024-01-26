9dda274ef448252221fff5297e2aac82bf215e17
Akuna Joshua <akunajoshua@gmail.com>
2024-01-26 18:34:49 +0100

    initial 0-simple_helper_function.py file commit

---
+++ 0x00-pagination/0-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Main file
+"""
+
+index_range = __import__("0-simple_helper_function").index_range
+
+res = index_range(1, 7)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=15)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=2)
+print(type(res))
+print(res)
---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def index_range(page, page_size):
+    """
+    the function takes in two integers page and page_size and returns
+    a tuple of size 2 containing a start index and an end index
+    corresponding to the range of indexes to return in a list.
+    """
+    begin, end = 0, 0
+
+    for i in range(page):
+        begin = end
+        end += page_size
+
+    return (begin, end)
