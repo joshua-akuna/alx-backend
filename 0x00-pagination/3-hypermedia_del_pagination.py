0cff5f415e279394296d71f5fd3bcbb01530ac72
Akuna Joshua <akunajoshua@gmail.com>
2024-01-26 20:00:28 +0100

    3-hypermedia_del_pagination.py: pycodestyle

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -51,10 +51,10 @@ class Server:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: dataset[i] for i in range (len(dataset))
+                i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
-    
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Returns a dictionary containing the following key-value pairs:
