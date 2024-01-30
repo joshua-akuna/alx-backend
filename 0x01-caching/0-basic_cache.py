d724a7c0497772d452f67347c1c81317a52dd2fc
Akuna Joshua <akunajoshua@gmail.com>
2024-01-30 11:36:58 +0100

    0-basic_cache.py : code refactored

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,13 +9,13 @@ class BasicCache(BaseCaching):
     """
     Implements a basic cache
     """
-    def put(self, k, v):
+    def put(self, key, item):
         """
         puts a key value pair into the cache
         """
-        self.cache_data[k] = v
+        self.cache_data[key] = item
 
     def get(self, k):
         """get a value from the cache based on the key arg
         """
-        return self.cache_data.get(k)
+        return self.cache_data.get(key)
