c1e0caf994afb61866f14296a60732bd9d38cffe
Akuna Joshua <akunajoshua@gmail.com>
2024-02-06 17:50:36 +0100

    0-app.py files commit

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""The module defines a flask web app"""
+
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+@app.route("/")
+def hello():
+    """renders an html page on screen for path /"""
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
