37602c835c80609b6f1ae36227e995d96225c226
Akuna Joshua <akunajoshua@gmail.com>
2024-02-06 20:04:39 +0100

    3-app.py : imported the request module

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """The module defines a flask web app"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
