c5495eaedeff41b7d1952117e24762133efdf7fc
Akuna Joshua <akunajoshua@gmail.com>
2024-02-06 18:20:38 +0100

    2-app.py files commit for supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""The module defines a flask web app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """configuration for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask("__name__")
+app.config.from_object(Config)
+babel = Babel()
+
+
+@babel.localeselector
+def get_locale():
+    """Uses the request.accept_languages to determine
+        the best match with the supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """renders an html page on screen for path /"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
