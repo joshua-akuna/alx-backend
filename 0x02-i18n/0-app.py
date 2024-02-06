c4d09fb4cbd89afb9c69c2f2a244ca967e15ff3b
Akuna Joshua <akunajoshua@gmail.com>
2024-02-06 17:54:55 +0100

    0-app.py : pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask("__name__")
 
+
 @app.route("/")
 def hello():
     """renders an html page on screen for path /"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
